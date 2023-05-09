e31fb76022819ac15a3f34b60e104b4eec3642c6
Nikhil Unni <nikhil.unni@cortex.io>
2023-05-09 15:24:59 -0500

    upgrade resteasy to support jakarta

--- src/main/java/org/gitlab4j/api/http/OkHttpResteasyClientFactory.java
+++ src/main/java/org/gitlab4j/api/http/OkHttpResteasyClientFactory.java
@@ -4,20 +4,17 @@ import net.ltgt.resteasy.client.okhttp3.OkHttpClientEngine;
 import okhttp3.OkHttpClient;
 import org.gitlab4j.api.utils.JacksonJson;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
 public class OkHttpResteasyClientFactory {
     public static ResteasyClient getClient(OkHttpClient client) {
         ResteasyProviderFactory instance = ResteasyProviderFactory.getInstance();
-        if (!instance.getClassContracts().containsKey(JacksonJson.class)) {
-            instance.registerProvider(JacksonJson.class);
-        }
-
+        instance.registerProvider(JacksonJson.class);
         RegisterBuiltin.register(instance);
 
-        return new ResteasyClientBuilder().providerFactory(instance)
+        return new ResteasyClientBuilderImpl().providerFactory(instance)
             .httpEngine(new OkHttpClientEngine(client))
             .build();
     }
