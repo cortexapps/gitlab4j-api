1251a70d6b9ef9c55d0c1c0222be11b5f8cb4812
Aditya Bansal <aditya.bansal93@gmail.com>
2023-05-16 20:29:32 -0700

    final fix

--- src/main/java/org/gitlab4j/api/http/OkHttpResteasyClientFactory.java
+++ src/main/java/org/gitlab4j/api/http/OkHttpResteasyClientFactory.java
@@ -8,19 +8,33 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.internal.LocalResteasyProviderFactory;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 public class OkHttpResteasyClientFactory {
     public static ResteasyClient getClient(OkHttpClient client) {
         // https://docs.jboss.org/resteasy/docs/5.0.1.Final/userguide/html/RESTEasy_Client_Framework.html#jetty_client
-        return ((ResteasyClientBuilder) ClientBuilder
-            .newBuilder()
-            .property(ClientProperties.FEATURE_AUTO_DISCOVERY_DISABLE, true)
-            .property(ClientProperties.METAINF_SERVICES_LOOKUP_DISABLE, true)
-            .register(JacksonJson.class)
-            .register(JacksonFeature.class)
-            .register(MultiPartFeature.class))
+        LocalResteasyProviderFactory factory = new LocalResteasyProviderFactory();
+        if (!factory.isEnabled(JacksonFeature.class)) {
+            factory.addFeature(JacksonFeature.class);
+        }
+
+        if (!factory.isEnabled(MultiPartFeature.class)) {
+            factory.addFeature(MultiPartFeature.class);
+        }
+
+        ResteasyProviderFactory.setInstance(factory);
+        ResteasyProviderFactory instance = ResteasyProviderFactory.getInstance();
+
+        if (!instance.isRegistered(JacksonJson.class)) {
+            instance.registerProvider(JacksonJson.class);
+        }
+
+        RegisterBuiltin.register(instance);
+
+        return new ResteasyClientBuilderImpl().providerFactory(instance)
             .httpEngine(new OkHttpClientEngine(client))
             .build();
     }
